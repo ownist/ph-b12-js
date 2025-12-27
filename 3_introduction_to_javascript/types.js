85d043b36e72a5aa796a363cad071d6a6812bede
alshahedbishwas <shahedevise@gmail.com>
2025-12-28 01:18:30 +0600

    13-6 Variable data types, Numeric, String, Boolean

---
+++ 3_introduction_to_javascript/types.js
@@ -0,0 +1,10 @@
+var name = "AL Shahed";
+// console.log(name);
+
+var address = "andor-killah bandorbon with some katabon";
+// console.log(address);
+
+var country = "Bangladesh";
+
+var isRich = true;
+console.log(typeof isRich);
