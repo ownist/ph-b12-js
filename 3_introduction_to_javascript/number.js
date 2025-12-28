9ec132fe83c1781e12a4bbc064c085a45c9f8714
alshahedbishwas <shahedevise@gmail.com>
2025-12-28 17:49:04 +0600

    13-8 Know the fundamentals of JS Numbers

---
+++ 3_introduction_to_javascript/number.js
@@ -0,0 +1,20 @@
+var orangePRice = 20;
+var chocolate = 0.5;
+var applePrice = parseFloat("202.5");
+
+// console.log(typeof orangePRice);
+// console.log(applePrice);
+// console.log(orangePRice + applePrice);
+// console.log(typeof orangePRice);
+// console.log(typeof applePrice);
+
+console.log(typeof applePrice);
+console.log(applePrice + orangePRice);
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+console.log(parseFloat(total.toFixed(2)));
+
+const num = -50 / 0;
+console.log(num);
