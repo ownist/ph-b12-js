e48089b12f8e4852bf9a22dc9627b5defe8dbdbd
alshahedbishwas <shahedevise@gmail.com>
2025-12-29 00:44:35 +0600

    14_4 Conditional branching if Else Condition

---
+++ 3_introduction_to_javascript/module_14/if.js
@@ -0,0 +1,16 @@
+if (33 < 15) {
+  console.log("i have a smaller value");
+}
+
+var biriyaniPrice = 500;
+
+if (biriyaniPrice < 300) {
+  console.log("mama give me a biriyani");
+}
+
+var weight = 20;
+if (weight <= 20) {
+  console.log("i will carry it by myself");
+} else {
+  console.log("i will rent a rickshaw");
+}
