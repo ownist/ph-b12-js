2c8cfcf778be967958dd267635e1c079fe449bb7
alshahedbishwas <shahedevise@gmail.com>
2025-12-31 02:31:57 +0600

    14_9 (advanced) Logical Not Operator

---
+++ 3_introduction_to_javascript/module_14/shortcut.js
@@ -0,0 +1,13 @@
+const isLeader = true;
+if (isLeader) {
+  console.log("leader asche.. chair charo");
+} else {
+  console.log("vitore dhukte dibi na");
+}
+
+const isPassed = false;
+if (!isPassed) {
+  console.log("tore biya diya dimu");
+} else {
+  console.log("tui amr noyoner moni");
+}
