fc0318c259a45a182977127ab6e95c4a6aa9307b
alshahedbishwas <shahedevise@gmail.com>
2025-12-31 00:51:53 +0600

    14_7 (optional) Nested if-else condition

---
+++ 3_introduction_to_javascript/module_14/nested.js
@@ -0,0 +1,15 @@
+const money = 15;
+
+if (money >= 300) {
+  console.log("broh! u are rich kid!!");
+} else {
+  if (money >= 100) {
+    console.log("tuin gorib o na.. boroloxa-aw na");
+  } else {
+    if (money >= 10) {
+      console.log("dst.. tui kola kha ar calcium barah ja vag");
+    } else {
+      console.log("oi manger nati ja vag.. tui amr kache tk dhar chaibi na");
+    }
+  }
+}
