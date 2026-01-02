3a00d8d5f0dd58eb965688b4b87e40e88515221b
alshahedbishwas <shahedevise@gmail.com>
2026-01-02 23:50:02 +0600

    15_2 Introduction to While Loop

---
+++ 3_introduction_to_javascript/module_15/js_loop/while.js
@@ -0,0 +1,13 @@
+const number = 7;
+
+if (number < 10) {
+  console.log("choto ekta number paichi");
+} else {
+  console.log("something new is cooking");
+}
+
+let num = 0;
+while (num < 5) {
+  console.log("looooooooping....", num);
+  num++;
+}
