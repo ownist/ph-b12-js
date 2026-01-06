b3b7158fa75686d7c77afbed339713e98b6c69e5
alshahedbishwas <shahedevise@gmail.com>
2026-01-06 22:39:05 +0600

    15_8 (optional) Introduction to Do While Loop

---
+++ 3_introduction_to_javascript/module_15/js_loop/do_while.js
@@ -0,0 +1,16 @@
+// almost use korbo na
+
+let i = 10;
+
+while (i > 1) {
+  console.log(i);
+  i--;
+}
+
+console.log("---- break ---");
+
+let n = 10;
+do {
+  console.log(n);
+  n++;
+} while (n <= 5);
