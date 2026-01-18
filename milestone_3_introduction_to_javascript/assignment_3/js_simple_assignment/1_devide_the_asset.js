dabbc8e75d836a1844dfc8a19089e116d5018fd5
codebyshahed <shahedevise@gmail.com>
2026-01-18 20:21:27 +0600

    assignment feat: assignment-03 js assignment er first 3 ta problem solve kore felsi. aro 3 ta baki ache

---
+++ milestone_3_introduction_to_javascript/assignment_3/js_simple_assignment/1_devide_the_asset.js
@@ -0,0 +1,7 @@
+/**
+ * Problem - 01  Divide the Asset
+ */
+
+let area = 2060;
+let devideArea = area / 2;
+console.log(devideArea);
---
+++ milestone_3_introduction_to_javascript/assignment_3/js_simple_assignment/3_medicine_planner.js
@@ -0,0 +1,13 @@
+/**
+ * Problem 03 - Medicine Planner
+ */
+
+let lastDay = 6;
+
+for (let i = 1; i <= lastDay; i++) {
+  if (i % 3 === 0) {
+    console.log(`${i} - medicine`);
+  } else {
+    console.log(`${i} - reset`);
+  }
+}
