741a52e6ddbabebe0a5f4e2883f1f6820582bfd1
codebyshahed <shahedevise@gmail.com>
2026-01-27 16:45:41 +0600

    feat: 19-1 What are function and function syntax

---
+++ milestone_4_problem_solving_with_javascript/module_19/js_function/function.js
@@ -0,0 +1,30 @@
+const money = 100;
+
+/**
+ * function declaretion. ei functiontake output hisebe dekhte chaile eiitake call korte hobe
+ */
+function fanOffKor() {
+  console.log("jeikhane achos seikhan theke fan er switch er kache ja");
+  console.log("switch tate click kor");
+}
+// call this function
+fanOffKor();
+
+// break use kora hoise. cz duita alda jinis eita bujanor jonno
+console.log("------- break ------------");
+
+/**
+ * brush korar function
+ *
+ * function ke sudhu declare korsi. eitake output hisebe dekhte chaile ei fucntiontake call korte hobe
+ */
+function brushTeeth() {
+  console.log("brash ta ber kore hate ne");
+  console.log("brush e paste laga");
+  console.log("then paste e halka pani laga");
+  console.log("ekhn valovabe tor dat 2 min ghoshaghoshi kor");
+  console.log("ghoshaghoshi ses hoye gele kuli kor");
+  console.log("brush kora complete");
+}
+// brush korar funtion take call ortesi
+brushTeeth();
